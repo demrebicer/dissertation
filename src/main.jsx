209e672c7149a622014ca6f7d38c41461995c691
demrebicer <demrebicer33@gmail.com>
2024-07-19 00:57:31 +0100

    refactor: Remove unused imports and optimize stylesheets

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import RouterHandler from "./router";
 import { BrowserRouter } from "react-router-dom";
 
+import "./assets/styles/index.scss";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
